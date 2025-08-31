e9af03fcef5d14603108adffb556e053298ee800
NAKANO8 <hika110308@gmail.com>
2025-08-31 12:02:35 +0900

    chore: add .gitignore and remove tracked dist & node_modules

--- dist/creture.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Creature {
-    private name;
-    private hp;
-    constructor(name: string, hp: number);
-    getName(): string;
-    setName(name: string): void;
-    attack(): void;
-}
-//# sourceMappingURL=creture.d.ts.map
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import { Creature } from "./creture.js";
-const slime = new Creature("スライム", 100);
-console.log(slime.getName()); // スライム
-slime.attack(); // HPが5ポイント減少しました。
-console.log(slime.getName()); // スライム
-//# sourceMappingURL=index.js.map
