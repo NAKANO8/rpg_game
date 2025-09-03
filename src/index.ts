d85f51c4e4eabf3fd628e91bed5a5d2c4db09ef3
NAKANO8 <hika110308@gmail.com>
2025-09-03 10:13:04 +0900

    viewの追加

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { Swordsman } from "./classes/jobs/Swordsman.js";
 import { Magician } from "./classes/jobs/Magician.js";
 import { Goblin } from "./classes/monsters/Goblin.js";
 
-const hero = new Swordsman("剣士タロウ", 30);
-const mage = new Magician("魔術師ハナコ", 25);
-const goblin = new Goblin("ゴブリンA", 20);
+const hero = new Swordsman("剣士タロウ");
+const mage = new Magician("魔術師ハナコ");
+const goblin = new Goblin("ゴブリンA");
 
 hero.attack(goblin);
 mage.attack(goblin);
